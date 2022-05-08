404c71fcc5c8b728fd119638df995d1d4a9d5207
Princeyyyy <princemumo80@gmail.com>
2022-05-08 23:16:05 +0300

    finished login and registration screens

---
+++ app/src/main/java/com/example/androidcoursework/WorkoutsScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidcoursework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutsScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts_screen);
+    }
+}
