20f2d96a8006972df6f66dc6eeb5d7aa41308476
lewismuchoki <lewis.ruini@gmail.com>
2022-05-09 21:34:35 +0100

    added Bottom navBar

--- app/src/main/java/com/example/androidcoursework/MainActivity.java
+++ app/src/main/java/com/example/androidcoursework/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.androidcoursework;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.workouts);
+
+    }
+    WorkoutsFragment workoutsFragment = new WorkoutsFragment();
+    ProfileFragment profileFragment= new ProfileFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.workouts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container1, workoutsFragment).commit();
+                return true;
+
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container2, profileFragment).commit();
+                return true;
+
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container3, settingsFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
