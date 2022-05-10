59684444e351d35d3ddfee5768b5d574c42178f3
Princeyyyy <princemumo80@gmail.com>
2022-05-11 00:16:34 +0300

    finished splash screen

---
+++ app/src/main/java/com/example/androidcoursework/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+package com.example.androidcoursework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH = 3000;
+    Animation animation;
+
+    private TextView appName;
+    private ImageView appImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        appImage = findViewById(R.id.appImage);
+        appName = findViewById(R.id.appName);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        appImage.setAnimation(animation);
+        appName.setAnimation(animation);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent;
+            intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH);
+    }
+}
