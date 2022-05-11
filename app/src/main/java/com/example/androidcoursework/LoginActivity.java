6d3e17d6f2f26d4d981de3522e2f6258f13ef770
Princeyyyy <princemumo80@gmail.com>
2022-05-11 23:45:05 +0300

    registration and login complete

--- app/src/main/java/com/example/androidcoursework/LoginActivity.java
+++ app/src/main/java/com/example/androidcoursework/LoginActivity.java
@@ -2,13 +2,89 @@ package com.example.androidcoursework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText logEmail;
+    private EditText logPassword;
+    private Button logBtn;
+    private TextView gotoRegister;
+    private TextView loginMessage;
+
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logEmail = findViewById(R.id.loginEmail);
+        logPassword = findViewById(R.id.loginPassword);
+        logBtn = findViewById(R.id.loginButton);
+        gotoRegister = findViewById(R.id.signupScreen);
+        progressBar = findViewById(R.id.progressbar);
+        loginMessage = findViewById(R.id.loginMessage);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        gotoRegister.setOnClickListener(this::onRegistrationClick);
+
+        logBtn.setOnClickListener(v -> {
+            String emailString = logEmail.getText().toString();
+            String passwordString = logPassword.getText().toString();
+
+            if (TextUtils.isEmpty(emailString)) {
+                logEmail.setError("Email is Required");
+            }
+
+            if (TextUtils.isEmpty(passwordString)) {
+                logPassword.setError("Password is Required");
+            } else {
+
+                loginMessage.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                logBtn.setEnabled(false);
+
+                mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        loginMessage.setVisibility(View.GONE);
+                        progressBar.setVisibility(View.GONE);
+                        logBtn.setEnabled(true);
+                    }
+                });
+            }
+
+        });
+    }
+
+    public void onRegistrationClick(View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
 }
--- app/src/main/java/com/example/androidcoursework/RegistrationActivity.java
+++ app/src/main/java/com/example/androidcoursework/RegistrationActivity.java
@@ -2,13 +2,109 @@ package com.example.androidcoursework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText regEmail;
+    private EditText regPassword;
+    private Button btnReg;
+    private TextView gotoLogin;
+    private TextView signupMessage;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference users;
+    private ProgressBar progressBar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        regEmail = findViewById(R.id.registrationEmail);
+        regPassword = findViewById(R.id.registrationPassword);
+        btnReg = findViewById(R.id.signupButton);
+        gotoLogin = findViewById(R.id.loginScreen);
+
+        progressBar2 = findViewById(R.id.progressbar2);
+        signupMessage = findViewById(R.id.signupMessage);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        gotoLogin.setOnClickListener(this::onLoginClick);
+
+        btnReg.setOnClickListener(v -> {
+            String email = regEmail.getText().toString();
+            String password = regPassword.getText().toString();
+
+            if (TextUtils.isEmpty(email)) {
+                regEmail.setError("Email is Required");
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                regPassword.setError("Password is Required");
+            } else {
+
+//                UserDetails userDetails = new UserDetails("null", "null", email);
+//                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                        signupMessage.setVisibility(View.VISIBLE);
+//                        progressBar2.setVisibility(View.VISIBLE);
+//                        btnReg.setEnabled(false);
+//
+//                        users = FirebaseDatabase.getInstance().getReference().child("user-details").child(mAuth.getCurrentUser().getUid());
+//
+//                        users.setValue(userDetails).addOnCompleteListener(task1 -> {
+//                            //Do nothing
+//                        });
+//                    } else {
+//                        Toast.makeText(RegistrationActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+//                        signupMessage.setVisibility(View.GONE);
+//                        progressBar2.setVisibility(View.GONE);
+//                        btnReg.setEnabled(true);
+//                    }
+//                });
+//                signupMessage.setVisibility(View.VISIBLE);
+//                progressBar2.setVisibility(View.VISIBLE);
+//                btnReg.setEnabled(false);
+//
+//                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    } else {
+//                        Toast.makeText(RegistrationActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+//                        signupMessage.setVisibility(View.GONE);
+//                        progressBar2.setVisibility(View.GONE);
+//                        btnReg.setEnabled(true);
+//                    }
+//                });
+                Toast.makeText(this, "It is working!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onLoginClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.stay);
     }
 }
