a8ac3641d989b1775edc7323553d1896182f23ae
Princeyyyy <princemumo80@gmail.com>
2022-05-11 23:55:10 +0300

    updated fragments

--- app/src/main/java/com/example/androidcoursework/WorkoutsFragment.java
+++ app/src/main/java/com/example/androidcoursework/WorkoutsFragment.java
@@ -14,9 +14,10 @@ public class WorkoutsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workouts, container, false);
+        View view = inflater.inflate(R.layout.fragment_workouts, container, false);
+
+        return view;
     }
 }
