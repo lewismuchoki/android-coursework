feb8456f2ac9db9621c6692c7aa3b55c02cf3db5
Princeyyyy <princemumo80@gmail.com>
2022-05-13 00:53:01 +0300

    test again

--- app/src/main/java/com/example/androidcoursework/ProfileFragment.java
+++ app/src/main/java/com/example/androidcoursework/ProfileFragment.java
@@ -16,7 +16,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //Test
+        //Test2
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return view;
