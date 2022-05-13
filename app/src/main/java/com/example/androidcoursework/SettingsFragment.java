da30a7496e763b6864fce2ebb1968ef786f3d8fa
Princeyyyy <princemumo80@gmail.com>
2022-05-13 14:42:29 +0300

    logout functionality works

--- app/src/main/java/com/example/androidcoursework/SettingsFragment.java
+++ app/src/main/java/com/example/androidcoursework/SettingsFragment.java
@@ -1,13 +1,20 @@
 package com.example.androidcoursework;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SettingsFragment extends Fragment {
+    private Button logOut;
 
     public SettingsFragment() {
         // require a empty public constructor
@@ -17,7 +24,23 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        logOut = view.findViewById(R.id.logout);
+
+        logOut.setOnClickListener(view1 -> new AlertDialog.Builder(getContext())
+                .setTitle("Workout App")
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(getContext(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+                })
+                .setNegativeButton("No", null)
+                .show());
 
         return view;
     }
 }
+
